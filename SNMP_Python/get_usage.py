b33070a90c5dc61d9481e0bb522451125bb9d6da
Daniel Gomes <daniel@avantsec.com.br>
2018-05-14 14:05:19 -0300

    Teste 2

--- SNMP_Python/get_usage.py
+++ SNMP_Python/get_usage.py
@@ -84,13 +84,13 @@ OID = ["1.3.6.1.2.1.25.3.3.1.2.1","1.3.6.1.2.1.25.3.3.1.2.2","1.3.6.1.2.1.25.2.3
 
 for value in OID:
 
-    if str(value) == "1.3.6.1.2.1.25.2.3":
-        os.system("snmpwalk -v 2c -c {0} {1} {2} > mem.info".format(sys.argv[2],sys.argv[1],value))
+    #if str(value) == "1.3.6.1.2.1.25.2.3":
+        #os.system("snmpwalk -v 2c -c {0} {1} {2} > mem.info".format(sys.argv[2],sys.argv[1],value))
         #os.system("echo {0} > mem.info".format(HOLDER))
         #result = mem_process()
-        print(mem_process())
+        #print(mem_process())
 
-    elif str(value) == "1.3.6.1.2.1.25.3.3.1.2.1" or str(value) == "1.3.6.1.2.1.25.3.3.1.2.2" or str(value) == "1.3.6.1.4.1.25461.2.1.2.3.3.0":
+    if str(value) == "1.3.6.1.2.1.25.3.3.1.2.1" or str(value) == "1.3.6.1.2.1.25.3.3.1.2.2" or str(value) == "1.3.6.1.4.1.25461.2.1.2.3.3.0":
     #else:    
         result = str(os.popen("snmpget -v 2c -c {0} {1} {2} | cut -c52-100".format(sys.argv[2],sys.argv[1],value)).read())
         print(result)
