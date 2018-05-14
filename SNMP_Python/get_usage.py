831724eced362af63790ebe738623166ebe89b8d
Daniel Gomes <daniel@avantsec.com.br>
2018-05-14 14:32:35 -0300

    Teste Final parte 1?

--- SNMP_Python/get_usage.py
+++ SNMP_Python/get_usage.py
@@ -89,32 +89,30 @@ for value in OID:
         #os.system("echo {0} > mem.info".format(HOLDER))
         result = mem_process()
         result = result.strip('\n')
-        print(result)
-        #print(mem_process(), end='')
+        #print(result)
 
     elif str(value) == "1.3.6.1.2.1.25.3.3.1.2.1":
     #else:    
         result = str(os.popen("snmpget -v 2c -c {0} {1} {2} | cut -c50-100".format(sys.argv[2],sys.argv[1],value)).read())
         #print(result, end='')
         result = result.strip('\n')
-        print(result)
+        #print(result)
     
     elif str(value) == "1.3.6.1.2.1.25.3.3.1.2.2":
         result = str(os.popen("snmpget -v 2c -c {0} {1} {2} | cut -c50-100".format(sys.argv[2],sys.argv[1],value)).read())
         #print(result, end='')
         result = result.strip('\n')
-        print(result)
+        #print(result)
 
     elif str(value) == "1.3.6.1.4.1.25461.2.1.2.3.3.0":
         result = str(os.popen("snmpget -v 2c -c {0} {1} {2} | cut -c54-100".format(sys.argv[2],sys.argv[1],value)).read())
         #print(result, end='')
         result = result.strip('\n')
-        print(result)
+        #print(result)
         
-
-    #with open("result", "a") as out:
+    with open("result", "a") as out:
         #os.system("echo '{0};' > result".format(result))
-    #    out.write(result+';')
+        out.write(result+';')
 
 #activeEth = []
 #for iterator in range(1,301):
