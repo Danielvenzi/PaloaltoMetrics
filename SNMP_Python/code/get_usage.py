9e7786fba8b5316a4b9c453a199139c91dc0cb9f
Daniel Gomes <daniel@avantsec.com.br>
2018-05-16 10:27:19 -0300

    Debug 4

--- SNMP_Python/code/get_usage.py
+++ SNMP_Python/code/get_usage.py
@@ -72,6 +72,7 @@ def intStatus():
         print(line)
         if len(line) == 21:
             print("Chegou 1")
+            print(len(line))
             if line[4]+line[5]+line[6]+line[7]+line[8]+line[9]+line[10]+line[11]+line[12]+line[13]+line[14]+line[15]+line[16]+line[17]+line[18]+line[19]+line[20] == "<status>up</status>":
                 print("Chegou 2")
                 Bolean = 1
